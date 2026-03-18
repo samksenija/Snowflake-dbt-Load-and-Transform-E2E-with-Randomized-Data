592d9f92691bddce027cb379e463be7949cd728f
ksenijasam <ksenija2702@gmail.com>
2026-03-18 10:38:31 +0100

    title clarification & testing out AI extract

--- LT/create_stage_and_process_pdfs.sql
+++ LT/create_stage_and_process_pdfs.sql
@@ -13,3 +13,14 @@ COMMENT = 'Stage for storing and processing PDF documents';
 
 --Show upload result
 LIST @my_pdf_stage;
+
+
+SELECT AI_EXTRACT(
+  file => TO_FILE('@my_pdf_stage','transaction_REF169.pdf'),
+  responseFormat => [
+    ['credit', 'What is the credit value?'], 
+    ['debit', 'What is the debit value?'],
+    ['account_description', 'What is the account description?'],
+    ['date', 'What is the date of the transaction?'],
+    ['reference', 'What is the reference?']]
+);
