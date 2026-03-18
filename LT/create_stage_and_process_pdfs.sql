23a01dbd482558d88b5b1ca4c8b5afcac4f91b83
ksenijasam <ksenija2702@gmail.com>
2026-03-18 10:51:27 +0100

    wrapper function; testing wrapper function

--- LT/create_stage_and_process_pdfs.sql
+++ LT/create_stage_and_process_pdfs.sql
@@ -14,7 +14,7 @@ COMMENT = 'Stage for storing and processing PDF documents';
 --Show upload result
 LIST @my_pdf_stage;
 
-
+--Testing out AI extract
 SELECT AI_EXTRACT(
   file => TO_FILE('@my_pdf_stage','transaction_REF169.pdf'),
   responseFormat => [
@@ -24,3 +24,28 @@ SELECT AI_EXTRACT(
     ['date', 'What is the date of the transaction?'],
     ['reference', 'What is the reference?']]
 );
+
+--Wrapper function to extract data from documents
+CREATE OR REPLACE FUNCTION extract_document_data(
+    stage_name STRING,
+    file_path STRING
+)
+RETURNS VARIANT
+LANGUAGE SQL
+AS $$
+    SELECT AI_EXTRACT(
+      file => TO_FILE(stage_name, file_path),
+      responseFormat => [
+        ['credit', 'What is the credit value?'], 
+        ['debit', 'What is the debit value?'],
+        ['account_description', 'What is the account description?'],
+        ['date', 'What is the date of the transaction?'],
+        ['reference', 'What is the reference?']]
+    ):response
+$$;
+
+--Test function on the available PDF files in stage
+SELECT
+    RELATIVE_PATH,
+    extract_document_data('@my_pdf_stage', RELATIVE_PATH) as EXTRACTED_DATA
+FROM DIRECTORY('@my_pdf_stage');
